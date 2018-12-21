351c3b7e6954c88e05bfab6f32a1748a3c6d6233
seventeen <1253478653@qq.com>
2018-12-22 00:02:43 +0800

    地图初始化

--- src/pages/common/orderDetail.js
+++ src/pages/common/orderDetail.js
@@ -11,13 +11,27 @@ export class orderDetail extends Component {
           part="orderDetail"/>
         <div className="order-detail-map">
           <Button type="primary" style={{margin: 20}}>原始轨迹</Button>
-          <div className="map-show">
-          
+          <div className="map-container" id="map-container">
+            
           </div>
         </div>
       </div>
     )
   }
+  componentDidMount () {
+    this.renderMap()
+  }
+  renderMap () {
+    let map = new window.BMap.Map("map-container");
+    map.centerAndZoom(new window.BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别  
+    map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
+    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+    var top_right_navigation = new window.BMap.NavigationControl({anchor: window.BMAP_ANCHOR_TOP_RIGHT, type: window.BMAP_NAVIGATION_CONTROL_RIGHT});
+    this.addControl(map, top_right_navigation); // 新增控件
+  }
+  addControl (map, top_right_navigation) {
+		map.addControl(top_right_navigation); 
+	}
 }
 
 export default orderDetail
